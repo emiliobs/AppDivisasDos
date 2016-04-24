7b81b807d5424ca297da0fb76acd2dfa2530672d
emilio barrera <barrera_emilio@hotmail.com>
2016-04-25 00:04:45 +0200

    Add project files.

---
+++ AppDivisasDos/AppDivisasDos/DivisasPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace AppDivisasDos
+{
+    public partial class DivisasPage : ContentPage
+    {
+        public DivisasPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
